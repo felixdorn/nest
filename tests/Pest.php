e5778eedb968eb4856e96975902d9982baa87d5f
Félix Dorn <github@felixdorn.fr>
2021-08-22 20:55:03 +0200

    chore: fix ci

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,2 @@
 <?php
 
-use Carbon\Carbon;
-
-expect()->extend('toMatchPeriod', function (string $start, string $end) {
-    expect($this->value->start->eq(Carbon::parse($start)))->toBeTrue();
-    expect($this->value->end->eq(Carbon::parse($end)))->toBeTrue();
-});
